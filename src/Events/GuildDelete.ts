b4ac89c1a78b09c8773e82e8d04443a01357b8d7
Boss-Discord2 <austajm4185@gmail.com>
2020-06-17 18:23:59 -0400

    i fucked up

---
+++ src/Events/GuildDelete.ts
@@ -0,0 +1,38 @@
+import {Jerry} from "../main";
+import { ICommandContext } from "../types";
+import { Guild } from "eris";
+const config = require("../../config.json"); 
+
+class GuildCreateHandler {
+    name: string;
+    constructor(){
+        this.name = "guildDelete";
+    }
+
+    async handle(this: Jerry, guild: Guild): Promise<void> {
+        let owner = this.users.get(guild.ownerID)
+
+        this.executeWebhook('722663743271469067', config.guildWebhook, { 
+
+            embeds: [
+             {
+                author: { 
+                    name: 'Guild Delete', 
+                    icon_url: this.user.avatarURL
+                }, 
+                color: this.defaultColor,
+                //@ts-ignore
+                description: `Removed from a guild!\n**Guild:** ${guild.name} (\`${guild.id})\`\n**Owner:** ${owner.username}#${owner.discriminator}\n**Members:** ${guild.members.size}\n**Guilds:** ${this.guilds.size}`,
+                timestamp: new Date 
+
+              }
+            ]
+
+
+
+
+    })
+
+    }
+}
+export default new GuildCreateHandler; 
