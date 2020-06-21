740abbf746a6beee519816051becc7abc7823380
Boss-Discord2 <austajm4185@gmail.com>
2020-06-21 15:15:04 -0400

    ready webhook

--- build/Events/Ready.js
+++ build/Events/Ready.js
@@ -10,6 +10,21 @@ class ReadyHandler {
     async handle() {
         this.editStatus("online", { name: "in the pool!", type: 0 });
         this.logger.success("Jerry", "Jerry has connected to Discord!");
+        this.executeWebhook('722663407743926303', config.guildWebhook, {
+            embeds: [
+                {
+                    author: {
+                        name: 'Ready',
+                        icon_url: this.user.avatarURL
+                    },
+                    color: this.defaultColor,
+                    //@ts-ignore
+                    description: `Connected to Discord!\n**Guilds:** ${callisto.guilds.size}\n**Users:** ${this.users.size}\n**Time:** ${logDate} (${logTime})`,
+                    timestamp: new Date
+                }
+            ]
+        });
+        return this.logger.success("Jerry", "Jerry has connected to Discord!");
     }
 }
 exports.default = new ReadyHandler;
