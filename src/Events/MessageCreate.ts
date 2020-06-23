cfb2445ce96b7d691dcb89f29721e07e54bb1f05
Boss-Discord2 <austajm4185@gmail.com>
2020-06-22 23:38:10 -0400

    jerry pecks a little more

--- src/Events/MessageCreate.ts
+++ src/Events/MessageCreate.ts
@@ -17,7 +17,7 @@ class MessageCreateHandler{
         const blacklistPeck = ["264445053596991498"];
         //@ts-ignore
         if(blacklistPeck.includes(msg.channel.guild.id)){canPeck = false;}
-        if((randomNumGenerator === 25 || randomNumGenerator === 50 || randomNumGenerator === 75 || randomNumGenerator === 100) && canPeck){msg.channel.createMessage(`GET PECKED ${msg.author.mention}!`)}
+        if((randomNumGenerator === 10 || randomNumGenerator === 25 || randomNumGenerator === 50 || randomNumGenerator === 75 || randomNumGenerator === 100) && canPeck){msg.channel.createMessage(`GET PECKED ${msg.author.mention}!`)}
         handleCommand(msg, this);
     }
 }
